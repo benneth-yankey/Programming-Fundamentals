713d2f535cc9797f3718f64248a731486ddfbc6d
benneth-yankey <benneth.yankey@turntabl.io>
2021-09-27 01:10:01 +0000

    declared variables

---
+++ ex-2-a.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+
+  // declare variables
+
+  int a{};           // integer
+  int* b = &a;       // pointer to integer
+  int &c = a;        // reference to integer
+  const int d = a;   // constant integer
+
+  return 0;
+}
