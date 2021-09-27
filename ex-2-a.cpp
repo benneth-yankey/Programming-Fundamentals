fcb3d9137f744391f4d66f56d301e7058e7b4eb4
benneth-yankey <benneth.yankey@turntabl.io>
2021-09-27 01:29:43 +0000

    performs actions on variables

--- ex-2-a.cpp
+++ ex-2-a.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
 
   // declare variables
@@ -7,7 +9,20 @@ int main() {
   int a{};           // integer
   int* b = &a;       // pointer to integer
   int &c = a;        // reference to integer
-  const int d = a;   // constant integer
+  const int d{};     // constant integer
+
+
+  // perform operations
+
+  a = 5;             // assign 5 to integer
+  *b = ++(*b);       // increment integer through pointer
+  c = ++c;           // increment through reference
+
+  cout << "The value of integer is: " << a << endl << endl;   // 7
+
+  cout << "What happens if you attempt to change the value of the constant?: d = 1?: " << endl;
+  cout << " Ans: A compile-time error: attempt to assign read-only d" << endl;
 
   return 0;
 }
+
